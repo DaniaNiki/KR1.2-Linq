91c304f22b4a8abf5103d1b3814f3950256ac7ef
daniil <dan1aniki@yandex.ru>
2024-03-25 22:28:58 +0300

    add database

---
+++ SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace KR1._2
+{
+    public class SchoolContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=university.db");
+    }
+}
